f9f391570761ab26d0cc551ce76baadc79b9f068
Alexandru-Burla <37006672+Alexandru-Burla@users.noreply.github.com>
2020-02-01 04:03:10 +0100

    [UPD] Menu

---
+++ Assets/Scripts/instruction.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class instruction : MonoBehaviour
+{
+    /*
+     * 
+     * Copier coller:
+     * Background
+     * OptionsMenu
+     * 
+     * dans le canvas contenant le game
+     * 
+     * Dans le bouton back ajouter le pannel du jeu dans le premier OnClick, function: GameObject.SetActive(bool)  et cocher la petite case 
+     * 
+     * si les options de son de marchent pas demander a DRU!
+     * 
+     * c'est tout normalement !
+     */
+}
