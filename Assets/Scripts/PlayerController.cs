13c059588f6eb0fe9b265638ee8c70321a25bf91
Arthur Lambert <39805498+ArthurLambert@users.noreply.github.com>
2020-02-01 21:30:29 +0100

    Somme more spaceship component added
    Sprite to be redesign and collider to be resised

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,23 +7,31 @@ public class PlayerController : MonoBehaviour
 {
     public float speed = 6.0f;
     public float rotSpeed = 1.0f;
-    public GameObject component;
+    public GameObject hull;
+    public GameObject machineGun;
+    public GameObject radar;
+    public GameObject reactor;
 
     private Vector2 moveDirection;
     private GameObject sprite;
     private Animator anim;
     private float actualSpeed = 0;
     private CharacterController controller;
-    private int state = 0; // 0 for wallking, 1 for turret, 2 for repairing ....
-    private BoxCollider componentCollider;
+    private int state = 0; // 0 for walking, 1 for turret, 2 for repairing hull, 3 for radar, 4 for reactor....
+    private BoxCollider hullCollider;
+    private SphereCollider machineGunCollider;
+    private BoxCollider radarCollider;
+    private BoxCollider reactorCollider;
 
     void Start()
     {
         sprite = this.gameObject.transform.GetChild(0).gameObject;
         anim = sprite.GetComponent<Animator>();
         controller = GetComponent<CharacterController>();
-        componentCollider = component.GetComponent<BoxCollider>();
-        Debug.Log("Coord component" + component.transform.position);
+        hullCollider = hull.GetComponent<BoxCollider>();
+        machineGunCollider = machineGun.GetComponent<SphereCollider>();
+        radarCollider = radar.GetComponent<BoxCollider>();
+        reactorCollider = reactor.GetComponent<BoxCollider>();
     }
 
     void Update()
@@ -48,13 +56,33 @@ public class PlayerController : MonoBehaviour
         }
         if (Input.GetButtonDown("Fire2")) {
             if (state == 0) {
-                if (componentCollider.bounds.Contains(transform.position + sprite.transform.TransformDirection(Vector3.right) * 0.7f)) {
+                if (hullCollider.bounds.Contains(transform.position + sprite.transform.TransformDirection(Vector3.right) * 0.7f))
+                {
                     state = 2;
+                    anim.SetBool("is_moving", false);
+                    anim.SetBool("is_repairing", true);
+                }
+                else if (machineGunCollider.bounds.Contains(transform.position + sprite.transform.TransformDirection(Vector3.right) * 0.7f))
+                {
+                    state = 1;
+                    anim.SetBool("is_moving", false);
+                    anim.SetBool("is_fireing", true);
+                }
+                else if (radarCollider.bounds.Contains(transform.position + sprite.transform.TransformDirection(Vector3.right) * 0.7f))
+                {
+                    state = 3;
+                    anim.SetBool("is_moving", false);
+                    anim.SetBool("is_repairing", true);
+                }
+                else if (reactorCollider.bounds.Contains(transform.position + sprite.transform.TransformDirection(Vector3.right) * 0.7f))
+                {
+                    state = 4;
                     anim.SetBool("is_moving", false);
                     anim.SetBool("is_repairing", true);
                 }
             } else if (state != 0) {
                 anim.SetBool("is_repairing", false);
+                anim.SetBool("is_fireing", false);
                 state = 0;
             }
         }
