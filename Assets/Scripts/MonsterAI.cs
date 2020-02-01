77bdbd416fb9e2d4497518593459f1d15a5c2378
FrostRing <alexandre1.lefevre@epitech.eu>
2020-02-02 00:23:28 +0100

    [ADD] Spiders are creepy and now move towards the !ship

--- Assets/Scripts/MonsterAI.cs
+++ Assets/Scripts/MonsterAI.cs
@@ -4,16 +4,18 @@ using UnityEngine;
 
 public class MonsterAI : MonoBehaviour
 {
+    public float speed;
+    public Transform[] waypoint;
     // Start is called before the first frame update
 
     void Start()
     {
-        
+        speed = 1f;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        transform.position = Vector2.MoveTowards(transform.position, waypoint[0].position, speed * Time.deltaTime);  
     }
 }
