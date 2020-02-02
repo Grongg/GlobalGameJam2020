8e9e71c627c7e2d296e955634631cf11473c3cc8
FrostRing <alexandre1.lefevre@epitech.eu>
2020-02-02 10:03:54 +0100

    [ADD] Shooting collider hitbox and sound on kill and shoot

--- Assets/Scripts/MonsterAI.cs
+++ Assets/Scripts/MonsterAI.cs
@@ -6,10 +6,13 @@ public class MonsterAI : MonoBehaviour
 {
     public float speed;
     public Transform[] waypoint;
+    private AudioSource DeathSound;
+    public ScoreHandler score;
     // Start is called before the first frame update
 
     void Start()
     {
+        DeathSound = GetComponent<AudioSource>();
         speed = Random.Range(1.0f, 2.0f);
     }
 
@@ -17,5 +20,17 @@ public class MonsterAI : MonoBehaviour
     void Update()
     {
         transform.position = Vector2.MoveTowards(transform.position, waypoint[0].position, speed * Time.deltaTime);  
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log(collision.gameObject.tag);
+        if (collision.gameObject.tag == "Bullet")
+        {
+            Destroy(collision.gameObject);
+            DeathSound.Play(0); // Not working to fix doesnt work because object is destroyed and he no liky
+            score.addPoints(100);
+            Destroy(this);
+            Destroy(gameObject, 1.2f);
+        }
     }
 }
--- Assets/Scripts/Shooting.cs
+++ Assets/Scripts/Shooting.cs
@@ -6,9 +6,14 @@ public class Shooting : MonoBehaviour
 {
     public Transform firepoint;
     public GameObject bulletPrefab;
+    private AudioSource shootSound;
     public KeyCode fireKey;
     public float bulletForce = 20f;
     // Update is called once per frame
+    void Start() 
+    {
+        shootSound = GetComponent<AudioSource>();
+    }
     void Update()
     {
         if (Input.GetKeyDown(fireKey))
@@ -19,6 +24,7 @@ public class Shooting : MonoBehaviour
 
     void Shoot()
     {
+        shootSound.Play(0);
         GameObject bullet = Instantiate(bulletPrefab, firepoint.position, firepoint.rotation);
         Rigidbody2D rb = bullet.GetComponent<Rigidbody2D>();
         rb.AddForce(firepoint.up * bulletForce, ForceMode2D.Impulse);
