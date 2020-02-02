4e34f442241c977b7a17b7ba1d7b61716e4d27b8
grong <lionel.de-sousa@epitech.eu>
2020-02-02 03:46:17 +0100

    [ADD] Score + merge score with main game

---
+++ Assets/Scripts/GameHandler.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameHandler : MonoBehaviour
+{
+    public ScoreHandler score;
+    void Start()
+    {
+        score.addPoints(1);    
+    }
+
+    void Update()
+    {
+        score.addPoints(1);
+    }
+}
---
+++ Assets/Scripts/ScoreHandler.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class ScoreHandler : MonoBehaviour
+{
+    private int startingScore = 0;
+    private int currentScore;
+    public TextMeshProUGUI scoretxt;
+
+    private void Start()
+    {
+        currentScore = startingScore;
+        displayScore();
+    }
+    public void setScore(int score)
+    {
+        currentScore = score;
+        displayScore();
+    }
+
+    public int getScore()
+    {
+        return currentScore;
+    }
+
+    public void displayScore()
+    {
+        scoretxt.text = "Score: " + currentScore.ToString();
+    }
+
+    public void resetScore()
+    {
+        setScore(0);
+        displayScore();
+    }
+
+    public void addPoints(int points)
+    {
+        currentScore += points;
+        displayScore();
+    }
+}
