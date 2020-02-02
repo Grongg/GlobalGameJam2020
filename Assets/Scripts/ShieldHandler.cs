4cc70f017091153608981aac3aa91201b372d09f
FrostRing <alexandre1.lefevre@epitech.eu>
2020-02-02 03:17:12 +0100

    [ADD] Collision and damage from monsters

--- Assets/Scripts/ShieldHandler.cs
+++ Assets/Scripts/ShieldHandler.cs
@@ -8,13 +8,15 @@ public class ShieldHandler : MonoBehaviour
     public HealthBar repairBar;
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        Debug.Log("collision");
-        healthBar.doDamage(10);
+        if (collision.gameObject.tag == "Monster")
+        {
+            healthBar.doDamage(10);
+            Destroy(collision.gameObject);
+        }
     }
     void Start()
     {
         //healthBar.displayHealth();
-        healthBar.doDamage(1);
-        repairBar.doRepair(3);
+//        repairBar.doRepair(1);
     }
 }
