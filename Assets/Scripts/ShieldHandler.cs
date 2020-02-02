3931edc33f86c073ee9e5a38ccc99a8c2c6fc089
FrostRing <alexandre1.lefevre@epitech.eu>
2020-02-02 03:38:45 +0100

    [ADD] Sound to impacts on shield

--- Assets/Scripts/ShieldHandler.cs
+++ Assets/Scripts/ShieldHandler.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class ShieldHandler : MonoBehaviour
 {
+    private AudioSource shieldDamaged;
     public HealthBar healthBar;
     public HealthBar repairBar;
     private void OnTriggerEnter2D(Collider2D collision)
@@ -12,10 +13,12 @@ public class ShieldHandler : MonoBehaviour
         {
             healthBar.doDamage(10);
             Destroy(collision.gameObject);
+            shieldDamaged.Play(0);
         }
     }
     void Start()
     {
+        shieldDamaged = GetComponent<AudioSource>();
         //healthBar.displayHealth();
 //        repairBar.doRepair(1);
     }
